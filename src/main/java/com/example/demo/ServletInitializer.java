ba10b9ed8cd35543890de902ed19d72f770aac73
TorresJ <TorresJ@RSNT-LPT-36.royalsystems.net>
2017-11-15 21:33:40 -0500

    primera subida

---
+++ src/main/java/com/example/demo/ServletInitializer.java
@@ -0,0 +1,30 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+//public class ServletInitializer extends SpringBootServletInitializer {
+//
+//	@Override
+//	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+//		return application.sources(DemoApplication.class);
+//	}
+//
+//}
+
+
+@SpringBootApplication
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(ServletInitializer.class);
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(ServletInitializer.class, args);
+    }
+
+}
